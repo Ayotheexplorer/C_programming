86aedc1afe1e14e65547cf7f7dfeaf436dc1265a
* <olashalomayobami@gmail.com>
2023-04-05 10:37:44 +0100

    Print Hello World

---
+++ Hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * printf: A function that prints a command
+ * Return: 0 (Succes)
+ */
+
+int main(void)
+{
+	printf("Hello World\n");
+	
+	return (0);
+}
