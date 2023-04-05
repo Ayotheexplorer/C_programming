55e666ad1ea4e763d07c18c0576a3a6dffe14104
* <olashalomayobami@gmail.com>
2023-04-05 10:50:16 +0100

    Addition of numbers

---
+++ Integers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 7;
+	int b = 8;
+	int c;
+	c = a + b;
+	
+	printf("The value of a is %d\n The value of b is %d\n The value of c is\n %d + %d = %d\n", a, b, a, b, c);
+
+	return (0);
+}
