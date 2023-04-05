89b5f6c86804ddb59b12b34c8470a144432105f1
* <olashalomayobami@gmail.com>
2023-04-05 11:34:07 +0100

    Adjustment to copy file

--- Basics-C/Integers.c
+++ Basics-C/Integers.c
@@ -1,16 +1,20 @@
-#include <stdio.h>                                              /**                                                              * main - Entry point                                            * @a: Integer one
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @a: Integer one
  * @b: Integer two
  * @c: Addition of integer one and two
  * Return: 0 (Success)
  */
 
-int main(void)                                                  {
-        int a = 7;
-        int b = 8;
-        int c;                                                          c = a + b;
+int main(void)
+{
+	int a = 7;
+	int b = 8;
+	int c;
+	c = a + b;
 
-        printf("The value of a is %d\n The value of b is %d\n Th
-e value of c is\n %d + %d = %d\n", a, b, a, b, c);
+	printf("The value of a is %d\n The value of b is %d\n The value of c is\n %d + %d = %d\n", a, b, a, b, c);
 
-        return (0);
+	return (0);
 }
